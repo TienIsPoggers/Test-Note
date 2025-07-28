db9d0bbbd3b8dcf2691275324408515504eb532d
TienIsPoggers <hangquoctien2008@gmail.com>
2025-07-28 14:23:52 +0700

    second commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
+import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
